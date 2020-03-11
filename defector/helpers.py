9736d7d122643fea532a262931a65ef628433e9d
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-03-11 10:38:30 +0100

    started implementing pymba

--- defector/helpers.py
+++ defector/helpers.py
@@ -1,8 +1,82 @@
 """A collection of helper functions used for the main program
 """
 
+from typing import Optional
+import cv2
+from pymba import Vimba, VimbaException, Frame
+
 
 def twice(x):
     """A basic example function used to show off pytest"""
 
     return x * 2
+
+
+# todo add more colours
+PIXEL_FORMATS_CONVERSIONS = {
+    'BayerRG8': cv2.COLOR_BAYER_RG2RGB,
+}
+
+
+def display_frame(frame: Frame, delay: Optional[int] = 1) -> None:
+    """
+    Displays the acquired frame.
+    :param frame: The frame object to display.
+    :param delay: Display delay in milliseconds, use 0 for indefinite.
+    """
+    print('frame {}'.format(frame.data.frameID))
+
+    # get a copy of the frame data
+    image = frame.buffer_data_numpy()
+
+    # convert colour space if desired
+    try:
+        image = cv2.cvtColor(image, PIXEL_FORMATS_CONVERSIONS[frame.pixel_format])
+    except KeyError:
+        pass
+
+    # display image
+    cv2.imshow('Image', image)
+    cv2.waitKey(delay)
+
+
+def get_frame(frames=1, cam=0):
+    """Get a frame from a vimba camera
+
+    Args:
+        frames (int): The amount of frames to get.
+            Defaults to 1
+        cam: The index(int) or camera_id(str)
+            Defaults to 0
+
+    Returns:
+        list of images
+
+    Raises:
+        Stuff
+    """
+
+    frames = []
+    with Vimba() as vimba:
+        camera = vimba.camera(cam)
+        camera.open()
+
+        camera.arm('SingleFrame')
+
+        # capture a single frame, more than once if desired
+        for i in range(frames):
+            try:
+                frames.append(camera.acquire_frame())
+                display_frame(frames[i], 0)
+            except VimbaException as e:
+                # rearm camera upon frame timeout
+                if e.error_code == VimbaException.ERR_TIMEOUT:
+                    print(e)
+                    camera.disarm()
+                    camera.arm('SingleFrame')
+                else:
+                    raise
+
+        camera.disarm()
+        camera.close()
+        return frames
