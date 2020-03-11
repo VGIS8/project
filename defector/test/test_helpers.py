20bcd1b41232c6e8ae338850ad7813294db52913
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-03-11 11:15:16 +0100

    ignore E402 flake8 for helpers test

--- defector/test/test_helpers.py
+++ defector/test/test_helpers.py
@@ -4,7 +4,7 @@
 import os
 
 # pymba complains in pytest if this isn't set
-os.environ["GENICAM_GENTL64_PATH"] = "N/A"
+os.environ["GENICAM_GENTL64_PATH"] = "N/A"  # noqa: E402 we have to do this before importing helpers
 
 from defector import helpers
 
