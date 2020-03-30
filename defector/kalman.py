e9b7be91e653410d2250e0ebded8eab92b035087
martinljep <39261101+martinljep@users.noreply.github.com>
2020-03-30 17:54:00 +0200

    added kalman.py

---
+++ defector/kalman.py
@@ -0,0 +1,21 @@
+"""provide function that implement kalman filter
+"""
+
+import numpy as np
+import cv2 as cv
+
+
+def init_kalman():
+
+    kf = cv.KalmanFilter(4, 2)
+    kf.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
+    kf.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
+    kf.processNoiseCov = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32) * 0.03
+    return kf
+
+
+def kalman(kf, measurement):
+
+    kf.correct(measurement)
+    tp = kf.predict()
+    return tp
